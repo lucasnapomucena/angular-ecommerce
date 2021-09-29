0b4be563524949d036893179741cad62b8d1827f
Lucas Napomucena <lucasnapomucena@gmail.com>
2021-09-29 14:22:11 -0300

    feat: adjust header and main style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   exports: [
@@ -31,7 +32,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatBadgeModule,
     MatInputModule,
     MatRadioModule,
-    MatTabsModule
+    MatTabsModule,
+    MatProgressBarModule
   ]
 })
 export class SharedModule {}
