35c929648601a4c26d69accff9762265b0786691
Lucas Napomucena <lucasnapomucena@gmail.com>
2021-10-04 13:46:57 -0300

    feat: add validator profile

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { select, Store } from '@ngrx/store';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 import { Product } from '../../core/models/product.model';
-import { GetItems } from '../../store/actions';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
@@ -15,8 +15,31 @@ export class PaymentComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   displayedColumns: string[] = ['image', 'title', 'price'];
   storeSubscription: Subscription;
+  shippingStatus: boolean = false;
+  profileStatus: boolean = true;
 
-  constructor(private store: Store<any>, private router: Router) {
+  checkoutForm = this.formBuilder.group({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+    ]),
+    document: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(\d{3}\.){2}\d{3}\-\d{2}$/)
+    ]),
+    phone: new FormControl('', Validators.required)
+  });
+
+  constructor(
+    private store: Store<any>,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
     this.storeSubscription = store.pipe(select('shop')).subscribe((state) => {
       if (state.cart.length <= 0) {
         this.router.navigate(['cart']);
@@ -30,5 +53,13 @@ export class PaymentComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.storeSubscription.unsubscribe();
+  }
+
+  onSubmit(): void {
+    if (!this.checkoutForm.valid) {
+      return;
+    }
+    this.profileStatus = false;
+    this.shippingStatus = true;
   }
 }
